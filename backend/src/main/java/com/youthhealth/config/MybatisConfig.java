268e533c96a2fd3d10d83850714ca2bdd37919e4
jonscoke <huangjiapeng159265@gmail.com>
2026-03-14 20:38:44 +0800

    解决跨域问题

--- backend/src/main/java/com/youthhealth/config/MybatisConfig.java
+++ backend/src/main/java/com/youthhealth/config/MybatisConfig.java
@@ -4,6 +4,6 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@MapperScan("com.youthhealth.modules")
+@MapperScan("com.youthhealth.modules.*.mapper")
 public class MybatisConfig {
 }
